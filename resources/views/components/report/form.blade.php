357b704b336709f9157565eee47b39aa7c57ae42
ThiccPan <ihilmiderian@gmail.com>
2023-09-09 14:04:20 +0700

    add report title form

--- resources/views/components/report/form.blade.php
+++ resources/views/components/report/form.blade.php
@@ -2,7 +2,10 @@
     <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
     <form action="/report" method="post">
         @csrf
+        <input type="text" name="title" id="title">
+        <br>
         <textarea name="report" id="report" cols="30" rows="10"></textarea>
+        <br>
         <button type="submit">submit report</button>
     </form>
 </div>
